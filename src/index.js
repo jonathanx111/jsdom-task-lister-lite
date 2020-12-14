dc486b474349541ef334f9df3a246304dac368e8
jonathanx111 <jonathanx111@gmail.com>
2020-12-13 21:52:13 -0500

    final

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const taskForm = document.querySelector("#create-task-form")
+  // append/attatch event listeners
+  taskForm.addEventListener("submit", createTask)
+
 });
+
+function createTask(e) {
+  e.preventDefault();
+  // prevent default submit action so it won't make get
+  // request and reload page
+debugger
+  const taskDescription = document.querySelector("#new-task-description")
+  const newLi = document.createElement("li")
+  const taskUl = document.querySelector("#tasks")
+
+  newLi.textContent = taskDescription.value 
+  taskUl.append(newLi)
+  newLi.append(dltBtn())
+  e.target.reset()
+  function dltBtn() {
+    const btn = document.createElement("BUTTON")
+    btn.textContent = "X"
+    btn.addEventListener('click', function removeTask() {
+      newLi.remove()
+      btn.remove()
+    })
+    return btn
+  }
+}
+
+
